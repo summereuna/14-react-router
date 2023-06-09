ea6be89d4473315155da03cacdf55435b3432e85
summereuna <summereuna@gmail.com>
2023-06-09 15:14:36 +0900

    Add home router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
